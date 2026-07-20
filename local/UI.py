2d4b18fce72cb3ffaf1bcc3e572b6683a8ab3da5
Olutomiwa Oyegbola <olutomiwa.oyegbola@gmail.com>
2026-07-20 12:22:20 -0500

    accepted changes during merger

--- local/UI.py
+++ local/UI.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import customtkinter as ctk
 from engine import GameMechanics
 
@@ -40,47 +39,4 @@ class WordleUI(ctk.CTk):
         
         #-----------------------UI--------------------------------#
         
-=======
-import customtkinter as ctk
-from engine import GameMechanics
-
-# GRAY = "grey"
-
-class WordUI(ctk.CTkFrame):
-    def __init__(self, len):
-        super().__init__()
-        
-        self.word_lenght = len
-        self.entries = []
-        
-        
-        for i in range (len):
-            entry = ctk.CTkEntry(self, fg_color = "grey", width = 40)
-            entry.grid(row=1, col = 1, padx = 5, pady=5)
-            self.entries.append(entry)
-        
-
-class ColorChoice():
-    def __init__(self, status):
-        super().__init__()
-        
-        self.status  = status
-        
-
-
-class WordleUI(ctk.CTk):
-    def __init__(self, secret_word):
-        super().__init__()
-        
-        self.title("Wordle")
-        self.geometry("400x400")
-        
-        self.secret_word = secret_word
-        self.word_len = len(secret_word)
-        self.mechanics = GameMechanics(word = secret_word)
-        
-        
-        #-----------------------UI--------------------------------#
-        
->>>>>>> 114f70e963a97da9045840a632b68feb191c455d
         self.cover_block = ctk.Ctk
